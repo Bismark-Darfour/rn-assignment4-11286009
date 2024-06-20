564f4a6426e4facd5444de181620f85e70544fb5
Bismark-Darfour <badarfour-oduro@st.ug.edu.gh>
2024-06-20 00:15:01 +0000

    Added app.js

---
+++ jobizz/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import SignUpScreen from './components/LogInScreen';
+import HomeScreen from './components/HomeScreen';
+
+
+const Stack = createStackNavigator();
+
+
+
+//export function
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
